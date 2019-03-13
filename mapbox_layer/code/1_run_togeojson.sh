aa15a3ee47cb8762d2e518c4ccf5e1a0ece8f51b
Kyle Barron <barronk@mit.edu>
2019-03-12 20:23:05 -0400

    Update code path for mapbox layer

--- mapbox_layer/code/1_run_togeojson.sh
+++ mapbox_layer/code/1_run_togeojson.sh
@@ -1,8 +1,9 @@
 #! /usr/bin/env bash
 
+source_data_dir=../../data
 data_dir=../data
 mkdir -p $data_dir/geojson
-for fname in $(ls $data_dir/gpx); do
-    togeojson $data_dir/gpx/$fname \
+for fname in $(ls $source_data_dir/gpx); do
+    togeojson $source_data_dir/gpx/$fname \
         > $data_dir/geojson/$(basename -s .gpx $fname).geojson
 done
