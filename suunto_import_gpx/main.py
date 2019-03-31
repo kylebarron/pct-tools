9279929b5f99b1223f06a807e16707e128d4ee5c
Kyle Barron <barronk@mit.edu>
2019-03-31 19:28:43 -0400

    Try using chrome options

--- suunto_import_gpx/main.py
+++ suunto_import_gpx/main.py
@@ -48,6 +48,11 @@ from time import sleep
     '--tags', type=str, help='Tags for routes. Input is used for all uploads.')
 def import_routes(gpx_paths, name, sync, public, desc, website, tags):
     print('starting Chrome')
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--window-size=1420,1080')
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')
     driver = webdriver.Chrome()
     print('started Chrome')
     print('signing in')
